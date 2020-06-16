a8a699562d8c4fc7fc4320a204c1f952c6cc858c
Aude Faucheux <aude53@gmail.com>
2020-06-16 22:50:49 +0100

    add option to mongoose connector

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
-
 const dbUrl = "mongodb://localhost:27017/";
 
 exports.connect = (url = dbUrl, opts = {}) => {
-  return mongoose.connect(url, { ...opts, useNewUrlParser: true });
+  return mongoose.connect(url, {
+    ...opts,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
