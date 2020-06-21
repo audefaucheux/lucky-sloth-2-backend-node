b8975678eee964997b986129d00b7b7a36d28b71
Aude Faucheux <aude53@gmail.com>
2020-06-21 12:29:23 +0100

    update patch and add delete user routes

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -4,6 +4,10 @@ const controllers = require("./user.controllers.js");
 const router = Router();
 
 router.route("/").get(controllers.getMany).post(controllers.createOne);
-router.route("/:id").get(controllers.getOne).put(controllers.updateOne);
+router
+  .route("/:id")
+  .get(controllers.getOne)
+  .patch(controllers.updateOne)
+  .delete(controllers.removeOne);
 
 module.exports = router;
